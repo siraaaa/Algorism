2bd8bed8c326d7d9d9951994ad752b4e1cb5bb88
siraaaa <30367776+siraaaa@users.noreply.github.com>
2018-09-19 02:20:09 +0900

    hw3. 문자열 A X 문자열 B 곱하는 것까지 완료

--- Algorism/src/Question3.java
+++ Algorism/src/Question3.java
@@ -2,41 +2,41 @@ import java.util.*;
 import java.io.*;
 public class Question3 {
 //2018.09.12
-//how many ones?
+//Q3. How many ones?
+//AxB = 111, 11111.. 가장 작은 C : 찾고자 하는 수 C는 111, 이 때 1의 개수는?
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		int t = num;
+		int i = 1; // 2부터 시작
 		
-		int n = sc.nextInt();
-		int c = 1;
-		int t = n;
-		
-		while(!hasAllOnes(t)){
-			c++;
-			t = n*c;
+		while(!hasAllOnes(t)){ //true일 때 무한루프, !true(=false)가 되면 루프 종료
+			i++;
+			t = num * i; //2부터 곱하면서 모두 1인지 아닌지 검사
 		}
-		
-		System.out.println(countOnes(t));
+		System.out.println(countOnes(t)); //t의 1의 개수를 세어서 출력
 	}
 	
+	//모두 1인지 아닌지 검사
 	public static boolean hasAllOnes(int n){
 		while(n!=0){
-			int t = n%10;
-			if(t!=1) return false;
-			n = n/10;
+			int a = n % 10;
+			if(a != 1){
+				return false;
+			}
+			n = n / 10;
 		}
 		return true;
-		
 	}
 	
 	public static int countOnes(int n){
-		int ret = 0;
-		while (n!=0){
-			ret++;
-			n /= 10;
+		int count = 0;
+		while(n != 0){
+			count++;
+			n = n/10;
 		}
-		return ret;
-		
+		return count;
 	}
-
 }
--- Algorism/src/Question3_hw.java
+++ Algorism/src/Question3_hw.java
@@ -1,42 +1,50 @@
 import java.util.*;
 import java.io.*;
 public class Question3_hw {
-//201
+//2018.09.19
 //Q3. How many ones?
 //AxB = 111, 11111.. 가장 작은 C : 찾고자 하는 수 C는 111, 이 때 1의 개수는?
+//C의 값이 integer, long으로 표현이 안되는 경우(10^50 정도의 숫자를 처리할 수 있는 프로그램)
+//string 으로 계산(유틸리티 쓰지말고 순수하게 문자열로만)
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		Scanner sc = new Scanner(System.in);
-		int num = sc.nextInt();
-		int t = num;
-		int i = 1; // 2부터 시작
+		String numA = sc.nextLine();
+		String t = numA;
+		String numB = sc.nextLine();
+		//문자열 곱셈하기
+		//String numB = "1"; // 2부터 시작
+		String tmp = "";
+		String zero ="";
+		ArrayList<String> list = new ArrayList<String>();
 		
-		while(!hasAllOnes(t)){ //true일 때 무한루프, !true(=false)가 되면 루프 종료
-			i++;
-			t = num * i; //2부터 곱하면서 모두 1인지 아닌지 검사
-		}
-		System.out.println(countOnes(t)); //t의 1의 개수를 세어서 출력
-	}
-	
-	//모두 1인지 아닌지 검사
-	public static boolean hasAllOnes(int n){
-		while(n!=0){
-			int a = n % 10;
-			if(a != 1){
-				return false;
+		int j = numB.length() - 1;
+		
+		//문자열끼리 곱하기, 23x12일 때 tmp에는 64, 032로 저장되므로 46,230으로 reverse하여 더해야 함.
+		while(j >= 0){
+			tmp += zero;
+			String b = String.valueOf(numB.charAt(j));
+			
+			for(int i = numA.length() - 1; i >= 0; i--){
+				tmp += (Integer.parseInt(b) * Integer.parseInt(String.valueOf(numA.charAt(i))));
 			}
-			n = n / 10;
+			//배열에 저장
+			list.add(reverse(tmp));
+			tmp = "";
+			zero += "0";
+			j--;
 		}
-		return true;
+		
+		System.out.println(list);
 	}
 	
-	public static int countOnes(int n){
-		int count = 0;
-		while(n != 0){
-			count++;
-			n = n/10;
+	public static String reverse(String s){
+		String reverseStr = "";
+		for(int i=s.length()-1; i >= 0; i--){
+			reverseStr += s.charAt(i);
 		}
-		return count;
+		return reverseStr;
 	}
+
 }
